db6cbf2b65f75a1757dd40368efc2def708712fa
Samantha Malone <sammac99@gmail.com>
2021-07-15 13:55:48 -0700

    added html routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require('fs');
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
