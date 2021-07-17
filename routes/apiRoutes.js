dac8a04ff2094ca087223448c04faf44b55c96ae
Samantha Malone <sammac99@gmail.com>
2021-07-17 12:24:01 -0700

    playing with routes to save

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,25 +4,26 @@ const fs = require('fs');
 const path = require('path');
 
 router.get("/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
         const oldData = JSON.parse(data);
+        res.send(oldData);
+        console.log(oldData);
         if (err) {
             res.status(500).send(err);
         }
-        res.send(oldData);
-    })
+    });
 });
 
 router.post("/notes", (req, res) => {
     const { newNote } = req;
     console.log(newNote);
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
         const oldData=JSON.parse(data);
         // add id to req.body, another key value pair
         const newNote=req.body;
         const newData=[newNote, ...oldData];
         res.send(newData);
-    fs.writeFile(path.join(__dirname, "../db/db.json"), "utf8", (err) => {
+    fs.writeFile("../db/db.json", "utf8", (err) => {
         if (err) {
             res.status(500).send(err);
         }
@@ -34,7 +35,7 @@ router.delete("/notes/:id", (req, res) => {
     // read db.json, filter and write db.json
     const { id } = req.params;
     let doNotDel;
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
         const oldData = JSON.parse(data);
         if (err) {
             res.status(500).send(err);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,9 +7,9 @@ router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
-router.get("/index", (req, res) => {
-    res.sendFile(pah.join(__dirname, "../public/index.js"));
-});
+// router.get("/index", (req, res) => {
+//     res.sendFile(pah.join(__dirname, "../public/index.js"));
+// });
 
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
