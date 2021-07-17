0a3af97e2e0f6b6b007f56357b1436bd7c23e0e7
Samantha Malone <sammac99@gmail.com>
2021-07-17 11:26:17 -0700

    adding push and send commands

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const { response } = require('express');
+const { note } = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
 
 router.get("/notes", (req, res) => {
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+        const newData = JSON.parse(data);
         if (err) {
             res.status(500).send(err);
         }
-        res.json(data);
+        res.send(newData);
     })
 });
 
@@ -21,12 +22,20 @@ router.post("/notes", (req, res) => {
         // add id to req.body, another key value pair
         const newNote=req.body;
         const newData=[newNote, ...oldData];
-    fs.writeFile
+        // res.json(newData);
+    fs.writeFile(note);
     })
 });
 
 router.delete("/notes/:id", (req, res) => {
-    // read db.json, filter and write db.json
+    // // read db.json, filter and write db.json
+    // fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+    //     if (err) {
+    //         res.status(500).send(err);
+    //     }
+    //     const
+
+    // })
     
 })
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const path = require('path');
 
+module.exports = router;
+
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
-})
+});
+
+router.get("/index", (req, res) => {
+    res.sendFile(pah.join(__dirname, "../public/index.js"));
+});
 
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
-})
-module.exports = router;
+});
