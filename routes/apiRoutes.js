f97e7ad904e5bbd4461e5e79dda4e1d250cf9c15
Samantha Malone <sammac99@gmail.com>
2021-07-17 11:41:29 -0700

    added del route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,37 +5,50 @@ const path = require('path');
 
 router.get("/notes", (req, res) => {
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-        const newData = JSON.parse(data);
+        const oldData = JSON.parse(data);
         if (err) {
             res.status(500).send(err);
         }
-        res.send(newData);
+        res.send(oldData);
     })
 });
 
 router.post("/notes", (req, res) => {
+    const { newNote } = req;
+    console.log(newNote);
     fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-        if (err) {
-            res.status(500).send(err);
-        }
         const oldData=JSON.parse(data);
         // add id to req.body, another key value pair
         const newNote=req.body;
         const newData=[newNote, ...oldData];
-        // res.json(newData);
-    fs.writeFile(note);
+        res.send(newData);
+    fs.writeFile(path.join(__dirname, "../db/db.json"), "utf8", (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
     })
 });
 
 router.delete("/notes/:id", (req, res) => {
-    // // read db.json, filter and write db.json
-    // fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-    //     if (err) {
-    //         res.status(500).send(err);
-    //     }
-    //     const
-
-    // })
-    
-})
+    // read db.json, filter and write db.json
+    const { id } = req.params;
+    let doNotDel;
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+        const oldData = JSON.parse(data);
+        if (err) {
+            res.status(500).send(err);
+        }
+        doNotDel = oldData.filter((note) => {
+            return note.id !== id;
+        });
+        console.log(oldData);
+        fs.writeFile("./db/db.json", JSON.stringify(doNotDel), (err) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+        });
+        res.send(doNotDel)
+    });  
+});
 module.exports = router;
